e6f7f65b0fa2a91cb4482817dbcbf32481ba1734
pseshadri9 <pavan.seshadri1@gmail.com>
2023-01-26 17:54:29 -0500

    correct dataloader

--- data_process/process_dataset.py
+++ data_process/process_dataset.py
@@ -4,7 +4,7 @@ import torch
 import os
 import pandas as pd
 import ast
-import constants
+from data_process import constants
 
 TRAIN = 'train'
 TEST = 'test'
@@ -16,7 +16,9 @@ NEXT_TOKEN = 'next_token'
 USER_ID = 'user_id'
 COLUMNS = [SEQUENCE, NEXT_TOKEN, USER_ID, SKIP]
 
-class DataModule(pl.LightningDataModule):
+FILE_EXT = '.csv'
+
+class RecDataModule(pl.LightningDataModule):
     def __init__(self, filepath, batch_size):
         super().__init__()
           
@@ -27,20 +29,23 @@ class DataModule(pl.LightningDataModule):
         self.setup()
   
     def prepare_data(self, data):
-        data.st.apply(lambda x: ast.literal_eval(x))
-        data.skip.apply(lambda x: ast.literal_eval(x))
+        data.st = data.st.apply(lambda x: ast.literal_eval(x))
+        data.skip = data.skip.apply(lambda x: ast.literal_eval(x))
         st = []
+        u_ids = []
         for st_it in data.st:
-            st_ = self.__class__._add_special_tokens(st_it)
+            u_ids.append(st_it[0])
+            st_ = self.__class__._add_special_tokens(st_it[1:])
             st.append(st_)
 
         dataset = TensorDataset(torch.tensor(st),
+                                torch.tensor(u_ids),
                                 torch.tensor(data.lst.tolist()),
                                 torch.tensor(data.a.tolist()),
                                 torch.tensor(data.buy.tolist()),
                                 torch.tensor(data.skip.tolist()))
         return DataLoader(dataset, 
-                          batch_size = self.batch_size)
+                          batch_size = self.batch_size, shuffle=True)
     @staticmethod
     def _add_special_tokens(st):
         if type(st) == list:
@@ -55,8 +60,8 @@ class DataModule(pl.LightningDataModule):
     def setup(self, stage=None):
         
         #Load and format dataset
-        dirs = {v.split['_'][1][:-3]: v for v in os.listdir(self.filepath)}
-
-        self.train_data = self.prepare_data(pd.read_csv(dirs[TRAIN]))
-        self.val_data = self.prepare_data(pd.read_csv(dirs[VAL]))
-        self.test_data = self.prepare_data(pd.read_csv(dirs[TEST]))
+        dirs = {v.split('_')[-1][:-4]: v for v in os.listdir(self.filepath) if v.endswith(FILE_EXT)}
+        print(dirs)
+        self.train_data = self.prepare_data(pd.read_csv(self.filepath + dirs[TRAIN]))
+        self.val_data = self.prepare_data(pd.read_csv(self.filepath + dirs[VAL]))
+        self.test_data = self.prepare_data(pd.read_csv(self.filepath + dirs[TEST]))
